d402c07ebe5e98d0c16333f46152ec36b787c3d1
Bronzato1 <thierry.langie@skynet.be>
2024-04-29 18:19:36 +0200

    Add project files.

---
+++ FunctionAppTesting/Function1.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionAppTesting
+{
+    public class Function1
+    {
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("Function1")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        {
+            _logger.LogTrace($">>>> Log Trace");
+            _logger.LogDebug($">>>> Log Debug");
+            _logger.LogInformation($">>>> Log Information");
+            _logger.LogWarning($">>>> Log Warning");
+            _logger.LogError($">>>> Log Error");
+            _logger.LogCritical($">>>> Log Critical");
+
+            _logger.LogInformation("C# HTTP trigger function processed a request!");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
